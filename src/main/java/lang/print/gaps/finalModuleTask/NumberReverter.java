e2870c6a22dc0fcd6e524cbc539b5006a9819e38
mordim3r <valent1n.bazylev@gmai.com>
2026-06-05 13:48:38 +0300

    add

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+
+        int hundred = number/100;
+        int tens = (number/10)%10;
+        int ones = number%10;
+        System.out.println(ones*100+tens*10+hundred);
     }
 }
